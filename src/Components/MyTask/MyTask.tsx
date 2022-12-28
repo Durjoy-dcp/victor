bb3cb9269845931cdbd933083b9346d006e00d31
Durjoy-dcp <unknownccxv@gmail.com>
2022-12-29 01:41:41 +0600

    card button

--- src/Components/MyTask/MyTask.tsx
+++ src/Components/MyTask/MyTask.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { ITasks } from "../../model/ITasks";
 import SingleTask from "../SingleTask/SingleTask";
-
+import { BiMessageSquareEdit } from "react-icons/bi";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { GrCompliance } from "react-icons/gr";
 const MyTask: React.FC = () => {
   const [mytasks, setMyTasks] = useState<ITasks[]>([] as ITasks[]);
   useEffect(() => {
@@ -15,8 +17,21 @@ const MyTask: React.FC = () => {
       <h1>My tasks</h1>
       <div className="container mx-auto m-2">
         <div className="grid md:grid-cols-3 grid-cols-1">
-          {mytasks.map((mytask, idx) => (
-            <SingleTask key={mytask._id}></SingleTask>
+          {mytasks.map((mytask) => (
+            <SingleTask key={mytask._id} mytask={mytask}>
+              <div className="flex flex-wrap justify-center items-center">
+                <button className="m-3">
+                  <BiMessageSquareEdit className="text-2xl text-gray-400 hover:text-gray-600" />
+                </button>
+
+                <button className="m-3">
+                  <RiDeleteBin5Line className="text-2xl text-gray-400 hover:text-gray-600" />
+                </button>
+                <button className="m-3">
+                  <GrCompliance className="text-xl text-red-100 hover:text-gray-600" />
+                </button>
+              </div>
+            </SingleTask>
           ))}
         </div>
       </div>
