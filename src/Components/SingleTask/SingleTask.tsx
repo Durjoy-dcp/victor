3f1164ab2303a2b1bad27947abdd8d52db73e14c
Durjoy-dcp <unknownccxv@gmail.com>
2022-12-29 02:58:43 +0600

    length addded

--- src/Components/SingleTask/SingleTask.tsx
+++ src/Components/SingleTask/SingleTask.tsx
@@ -14,7 +14,7 @@ const SingleTask: React.FC<ISingle> = ({ mytask, children }) => {
     <div className="p-2">
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <div className="flex justify-between">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h5>
           <p>{date}</p>
