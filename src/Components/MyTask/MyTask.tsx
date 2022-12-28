a4f153546b915b933cc3afe28fd2046824e5dd86
Durjoy-dcp <unknownccxv@gmail.com>
2022-12-29 02:11:59 +0600

    updating  complete

--- src/Components/MyTask/MyTask.tsx
+++ src/Components/MyTask/MyTask.tsx
@@ -4,13 +4,32 @@ import SingleTask from "../SingleTask/SingleTask";
 import { BiMessageSquareEdit } from "react-icons/bi";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { GrCompliance } from "react-icons/gr";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 const MyTask: React.FC = () => {
   const [mytasks, setMyTasks] = useState<ITasks[]>([] as ITasks[]);
+  const navigate = useNavigate();
   useEffect(() => {
     fetch("http://localhost:5000/mytasks")
       .then((res) => res.json())
       .then((data) => setMyTasks(data));
   }, []);
+
+  const handleToCompleted = (id: string): void => {
+    console.log(id);
+    fetch(`http://localhost:5000/completed?id=${id}`, {
+      method: "PATCH",
+      headers: { "Content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          toast.success("Marked as Completed");
+          navigate("/completedtask");
+        }
+      });
+  };
+
   console.log(mytasks);
   return (
     <div>
@@ -27,7 +46,10 @@ const MyTask: React.FC = () => {
                 <button className="m-3">
                   <RiDeleteBin5Line className="text-2xl text-gray-400 hover:text-gray-600" />
                 </button>
-                <button className="m-3">
+                <button
+                  className="m-3"
+                  onClick={() => handleToCompleted(mytask._id)}
+                >
                   <GrCompliance className="text-xl text-red-100 hover:text-gray-600" />
                 </button>
               </div>
